7e8eb1dd325cc0a69d1710dd5e0c077c45321715
Ryan Nelson <ryanpn20@gmail.com>
2017-11-03 14:15:37 -0400

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171103122450) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "urls", force: :cascade do |t|
     t.string "original"
     t.string "random_id"
